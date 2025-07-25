cf92fd40c299ac546b3e4ff293cceb1d686e29e2
anduq <85969507+anduq@users.noreply.github.com>
2025-07-25 09:30:09 +0900

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import MintForm from './components/MintForm';
+
+function App() {
+  return (
+    <div>
+      <h1>🖼️ 나만의 NFT 마켓</h1>
+      <MintForm />
+    </div>
+  );
+}
+
+export default App;
